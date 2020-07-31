0877a8e354bcd233b072935956d8df2d5854c637
Gabriel Broadwin Nongsiej <gnongsie@visa.com>
2020-07-31 10:34:45 +0530

    Adding NumericUtility.cs

---
+++ src/Resource/NumericUtility.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cybersource_rest_samples_dotnet.Resource
+{
+    public class NumericUtility
+    {
+        public static string LongRandom(int min, int max)
+        {
+            Random generator = new Random();
+            string r = generator.Next(min, max).ToString("D8");
+            return r;
+        }
+    }
+}
