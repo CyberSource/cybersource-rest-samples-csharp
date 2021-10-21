873a9268404b9084ac09d36d11c9de880e68ce52
snavinch <snavinch@visa.com>
2021-10-21 21:10:17 +0530

    + Upgrade to latest dependencies and added samples

---
+++ Source/Samples/Reporting/RetrievalSummaries/GetRetrievalSummaries.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+using CyberSource.Api;
+using CyberSource.Model;
+
+namespace Cybersource_rest_samples_dotnet.Samples.Reporting
+{
+	public class GetRetrievalSummaries
+	{
+		public static ReportingV3RetrievalSummariesGet200Response Run()
+		{
+			// QUERY PARAMETERS
+			string organizationId = "testrest";
+			var startTime = DateTime.ParseExact("2021-08-01T00:00:00Z", "yyyy-MM-ddTHH:mm:ssZ", CultureInfo.InvariantCulture);
+			var endTime = DateTime.ParseExact("2021-09-01T23:59:59Z", "yyyy-MM-ddTHH:mm:ssZ", CultureInfo.InvariantCulture);
+			try
+			{
+				var configDictionary = new Configuration().GetConfiguration();
+				var clientConfig = new CyberSource.Client.Configuration(merchConfigDictObj: configDictionary);
+
+				var apiInstance = new RetrievalSummariesApi(clientConfig);
+				ReportingV3RetrievalSummariesGet200Response result = apiInstance.GetRetrievalSummary(startTime, endTime, organizationId);
+				Console.WriteLine(result);
+				return result;
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("Exception on calling the API : " + e.Message);
+				return null;
+			}
+		}
+	}
+}
