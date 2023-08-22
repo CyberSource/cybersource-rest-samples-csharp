e33b5c998ae370ac3ef9b589c0bc864682c6f6b4
monu-kumar-visa <138751796+monu-kumar-visa@users.noreply.github.com>
2023-08-22 14:54:25 +0530

    network tokenization changes

---
+++ Source/Samples/NetworkToken/CreateInstrumentIdentifierEnrollForNetworkToken.cs
@@ -0,0 +1,42 @@
+﻿using CyberSource.Api;
+using CyberSource.Model;
+using System;
+
+namespace Cybersource_rest_samples_dotnet.Samples.NetworkToken
+{
+    public class CreateInstrumentIdentifierEnrollForNetworkToken
+    {
+        public static Tmsv2customersEmbeddedDefaultPaymentInstrumentEmbeddedInstrumentIdentifier Run()
+        {
+            var profileid = "93B32398-AD51-4CC2-A682-EA3E93614EB1";
+            string type = "enrollable card";
+            string cardNumber = "5204245750003216";
+            string cardExpirationMonth = "12";
+            string cardExpirationYear = "2025";
+            Tmsv2customersEmbeddedDefaultPaymentInstrumentEmbeddedInstrumentIdentifierCard card = new Tmsv2customersEmbeddedDefaultPaymentInstrumentEmbeddedInstrumentIdentifierCard(
+                    Number: cardNumber,
+                    ExpirationMonth: cardExpirationMonth,
+                    ExpirationYear: cardExpirationYear
+                );
+            var requestObj = new PostInstrumentIdentifierRequest(
+                    Type: type,
+                    Card: card
+                );
+            try
+            {
+                var configDictionary = new Configuration().GetConfiguration();
+                var clientConfig = new CyberSource.Client.Configuration(merchConfigDictObj: configDictionary);
+
+                var apiInstance = new InstrumentIdentifierApi(clientConfig);
+                Tmsv2customersEmbeddedDefaultPaymentInstrumentEmbeddedInstrumentIdentifier result = apiInstance.PostInstrumentIdentifier(requestObj, profileid);
+                Console.WriteLine(result);
+                return result;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception on calling the API : " + e.Message);
+                return null;
+            }
+        }
+    }
+}
---
+++ Source/Samples/NetworkToken/PaymentCredentialsFromNetworkToken.cs
@@ -0,0 +1,32 @@
+﻿using CyberSource.Api;
+using System;
+
+namespace Cybersource_rest_samples_dotnet.Samples.NetworkToken
+{
+    public class PaymentCredentialsFromNetworkToken
+    {
+        public static string Run(string TokenId = null)
+        {
+            var profileid = "93B32398-AD51-4CC2-A682-EA3E93614EB1";
+            if (null == TokenId)
+            {
+                TokenId = CreateInstrumentIdentifierEnrollForNetworkToken.Run().Id;
+            }
+            try
+            {
+                var configDictionary = new Configuration().GetConfiguration();
+                var clientConfig = new CyberSource.Client.Configuration(merchConfigDictObj: configDictionary);
+
+                var apiInstance = new TokenApi(clientConfig);
+                var result = apiInstance.PostTokenPaymentCredentials(TokenId, profileid);
+                Console.WriteLine(result);
+                return result;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception on calling the API : " + e.Message);
+                return null;
+            }
+        }
+    }
+}
