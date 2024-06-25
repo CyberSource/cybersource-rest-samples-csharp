af70663817a4250f0d17c67fee617afc5ddf3993
gaubansa <gaubansa@visa.com>
2024-06-25 15:35:51 +0530

    adding sample logs in tms

--- Source/Samples/TokenManagement/InstrumentIdentifier/CreateInstrumentIdentifierCard.cs
+++ Source/Samples/TokenManagement/InstrumentIdentifier/CreateInstrumentIdentifierCard.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using System.Globalization;
 
 using CyberSource.Api;
+using CyberSource.Client;
 using CyberSource.Model;
 
 namespace Cybersource_rest_samples_dotnet.Samples.TokenManagement
 {
     public class CreateInstrumentIdentifierCard
     {
+        public static void WriteLogAudit(int status)
+        {
+            var filePath = System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.ToString().Split('.');
+            var filename = filePath[filePath.Length - 1];
+            Console.WriteLine($"[Sample Code Testing] [{filename}] {status}");
+        }
+
         public static PostInstrumentIdentifierRequest Run()
         {
             var profileid = "93B32398-AD51-4CC2-A682-EA3E93614EB1";
@@ -29,11 +37,13 @@ namespace Cybersource_rest_samples_dotnet.Samples.TokenManagement
                 var apiInstance = new InstrumentIdentifierApi(clientConfig);
                 PostInstrumentIdentifierRequest result = apiInstance.PostInstrumentIdentifier(requestObj, profileid);
                 Console.WriteLine(result);
+                WriteLogAudit(apiInstance.GetStatusCode());
                 return result;
             }
-            catch (Exception e)
+            catch (ApiException e)
             {
                 Console.WriteLine("Exception on calling the API : " + e.Message);
+                WriteLogAudit(e.ErrorCode);
                 return null;
             }
         }
