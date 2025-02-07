77327d808fff98717c198423d796b5bb33f2a908
mahmishr <mahmishr@visa.com>
2025-02-07 16:51:46 +0530

    "samples"

---
+++ Source/Samples/FlexMicroform/GenerateCaptureContextAcceptCard.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CyberSource.Api;
+using CyberSource.Model;
+
+namespace Cybersource_rest_samples_dotnet.Samples.FlexMicroform
+{
+    class GenerateCaptureContextAcceptCard
+    {
+        public static void WriteLogAudit(int status)
+        {
+            var filePath = System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.ToString().Split('.');
+            var filename = filePath[filePath.Length - 1];
+            Console.WriteLine($"[Sample Code Testing] [{filename}] {status}");
+        }
+
+        public static String Run()
+        {
+            string clientVersion = "v2";
+
+            List<string> targetOrigins = new List<string>();
+            targetOrigins.Add("https://www.test.com");
+
+            List<string> allowedCardNetworks = new List<string>();
+            allowedCardNetworks.Add("VISA");
+            allowedCardNetworks.Add("MASTERCARD");
+            allowedCardNetworks.Add("AMEX");
+            allowedCardNetworks.Add("CARNET");
+            allowedCardNetworks.Add("CARTESBANCAIRES");
+            allowedCardNetworks.Add("CUP");
+            allowedCardNetworks.Add("DINERSCLUB");
+            allowedCardNetworks.Add("DISCOVER");
+            allowedCardNetworks.Add("EFTPOS");
+            allowedCardNetworks.Add("ELO");
+            allowedCardNetworks.Add("JCB");
+            allowedCardNetworks.Add("JCREW");
+            allowedCardNetworks.Add("MADA");
+            allowedCardNetworks.Add("MAESTRO");
+            allowedCardNetworks.Add("MEEZA");
+            List<string> allowedPaymentTypes = new List<string>();
+            allowedPaymentTypes.Add("CARD");
+            var requestObj = new GenerateCaptureContextRequest(
+                ClientVersion: clientVersion,
+                TargetOrigins: targetOrigins,
+                AllowedCardNetworks: allowedCardNetworks,
+                AllowedPaymentTypes: allowedPaymentTypes
+
+            );
+
+            try
+            {
+                var configDictionary = new Configuration().GetConfiguration();
+                var clientConfig = new CyberSource.Client.Configuration(merchConfigDictObj: configDictionary);
+
+                var apiInstance = new MicroformIntegrationApi(clientConfig);
+                String result = apiInstance.GenerateCaptureContext(requestObj);
+                Console.WriteLine(result);
+                WriteLogAudit(apiInstance.GetStatusCode());
+                return result;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception on calling the API : " + e.Message);
+                return null;
+            }
+        }
+    }
+}
---
+++ Source/Samples/FlexMicroform/GenerateCaptureContextAcceptCheck.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CyberSource.Api;
+using CyberSource.Model;
+
+namespace Cybersource_rest_samples_dotnet.Samples.FlexMicroform
+{
+    class GenerateCaptureContextAcceptCheck
+    {
+        public static void WriteLogAudit(int status)
+        {
+            var filePath = System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.ToString().Split('.');
+            var filename = filePath[filePath.Length - 1];
+            Console.WriteLine($"[Sample Code Testing] [{filename}] {status}");
+        }
+
+        public static String Run()
+        {
+            string clientVersion = "v2";
+
+            List<string> targetOrigins = new List<string>();
+            targetOrigins.Add("https://www.test.com");
+            List<string> allowedPaymentTypes = new List<string>();
+            allowedPaymentTypes.Add("CHECK");
+            var requestObj = new GenerateCaptureContextRequest(
+                ClientVersion: clientVersion,
+                TargetOrigins: targetOrigins,
+                AllowedPaymentTypes: allowedPaymentTypes);
+
+            try
+            {
+                var configDictionary = new Configuration().GetConfiguration();
+                var clientConfig = new CyberSource.Client.Configuration(merchConfigDictObj: configDictionary);
+
+                var apiInstance = new MicroformIntegrationApi(clientConfig);
+                String result = apiInstance.GenerateCaptureContext(requestObj);
+                Console.WriteLine(result);
+                WriteLogAudit(apiInstance.GetStatusCode());
+                return result;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception on calling the API : " + e.Message);
+                return null;
+            }
+        }
+    }
+}
