1e537189988adf29be3fb269ea1f8ca3e0f25ac4
Aastvik Goel <aastgoel@visa.com>
2025-09-04 14:17:00 +0530

    Model Names Updated in Samples

--- Source/Samples/AccountUpdater/RetrieveBatchStatus.cs
+++ Source/Samples/AccountUpdater/RetrieveBatchStatus.cs
@@ -19,7 +19,7 @@ namespace Cybersource_rest_samples_dotnet.Samples.AccountUpdater
 				var clientConfig = new CyberSource.Client.Configuration(merchConfigDictObj: configDictionary);
 
 				var apiInstance = new BatchesApi(clientConfig);
-				InlineResponse2009 result = apiInstance.GetBatchStatus(batchId);
+				InlineResponse20010 result = apiInstance.GetBatchStatus(batchId);
 				Console.WriteLine(result);
 			}
 			catch (Exception e)
